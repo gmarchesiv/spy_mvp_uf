55988e725c67bd72eae25f339f2e244701648140
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2025-12-05 11:09:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -177,6 +177,9 @@ def main():
             clean_vars(vars,varsApp)
             data_option_open(app,vars,params)
             
+            broadcasting_Alinear_label(varsLb,params) 
+            time.sleep(5)
+
             generar_label(params, varsLb,app)
 
             timeNow = datetime.now(params.zone).time()
@@ -186,6 +189,7 @@ def main():
                varsApp.flag_bloqueo_tiempo=True
         else:
             load_app_vars(app, varsApp)
+            broadcasting_Alinear_label(varsLb,params) 
         
         wallet_config(app, params, vars)
 
@@ -203,7 +207,7 @@ def main():
         del dia y se rompe al finalizar el trading day. 
         '''
         #---------------------------------------------------
-        broadcasting_Alinear_label(varsLb,params) 
+        
         vars.ready=True
         while True:
 
